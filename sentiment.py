9ac69e3f252a2312d467550ca8b196098e779195
Mehul Solanki <mehulsolanki45@gmail.com>
2018-06-24 23:57:05 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = '1mH1dGHI5OHQZFJEYRncFSWNP'
-consumer_secret = 'GZEu1pR6UvSuuW2sO6JyBaegbTPPkeXMGiEFfZNLqVuUaL70le'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '3104899826-IsgnoKKElfaJbEIhtUZ8hPbfmuUAsu0A35XP5EW'
-acces_token_secret = '05N6fDHfWecIdXj8DRrEsGT1fHzr9o1b0lA7W96gs5ALs'
+access_token = ''
+acces_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, acces_token_secret)
